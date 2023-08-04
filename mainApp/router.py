f95eceadfd47d1dc94e37fb486582d2bcd2137f8
arunkumar.palani <arunkumar.palani@neosoftmail.com>
2023-08-04 10:03:58 +0530

    changes for update

--- mainApp/router.py
+++ mainApp/router.py
@@ -15,7 +15,8 @@ accountKey = os.getenv("accountKey")
 containerName = os.getenv("containerName")
 connectionString = f"DefaultEndpointsProtocol=https;AccountName={accountName};AccountKey={accountKey};EndpointSuffix=core.windows.net"   
 router = APIRouter()
-myclient = MongoClient("mongodb://admin:admin123@20.219.77.42:27017")
+#myclient = MongoClient("mongodb+srv://arunkumardev2511:ag0x27igJALeaPr4@cluster0.mirxmsy.mongodb.net/")
+myclient = MongoClient("mongodb://admin:admin123@127.0.0.1:27017/")
 mydb = myclient["qapp"]
 userTable = mydb["User"]
 siteTable = mydb["Site"]
@@ -60,14 +61,15 @@ def login(user:Login):
     userData = userTable.find_one({"isActive":True,"mobile":username,"password":password})
     if not userData:
         raise HTTPException(status_code=403,details="Incorrect username or password")
-    token = JWT().encrypt({"id":userData.get("_id")})
+    userData["_id"] = str(userData.get("_id"))
+    token = JWT().encrypt({"id":str(userData.get("_id"))})
 
     return {"success":True,"data":{"token":token,"user":userData}}
 
 @router.post("/user",tags=["User"])
 def addUser(user:User, token: str = Depends(tokenAuthScheme)):
-    userData = verifyToken(token.credentials)
-    checkExist = dict(userTable.find_one({"$or":[{"mobile":user.mobile},{"email":user.email}]}))
+    #userData = verifyToken(token)
+    checkExist = userTable.find_one({"$or":[{"mobile":user.mobile},{"email":user.email}]})
     if checkExist:
         raise HTTPException(status_code=422,detail="Email or Mobile already exist")
     dataToInsert = {
@@ -79,10 +81,10 @@ def addUser(user:User, token: str = Depends(tokenAuthScheme)):
         "email":user.email,
         "isActive":True,
         "createdAt":datetime.now(),
-        "isAdmin":False,
+        "isAdmin":True,
         "password":user.password,
-        "sites":[],
-        "createdBy":userData.get("_id")
+        "sites":[]
+        #"createdBy":userData.get("_id")
     }
     #user["isActive"] = True 
     userTable.insert_one(dataToInsert)
@@ -91,19 +93,18 @@ def addUser(user:User, token: str = Depends(tokenAuthScheme)):
 
 @router.get("/user",tags=["User"])
 def getUser(page:int = 1, limit:int = 10, token: str = Depends(tokenAuthScheme)): 
-    verifyToken(token.credentials)
-    skip = (page - 1)*limit
-    print(skip)
+    verifyToken(token)
+    skip = (page - 1)*limit 
     data = list(userTable.find({"isActive":True,"isAdmin":False}).limit(limit).skip(skip)) 
     for el in data:
         el["_id"] = str(el["_id"])
-        if el.get("sites") and len(el.get("sites")) > 0:
-            el["sites"] = siteTable.find({"_id":{"$in":el["sites"]}})
+        #sif el.get("sites") and len(el.get("sites")) > 0:
+            #sel["sites"] = siteTable.find({"_id":{"$in":el["sites"]}})
     return {"success":True,"data":data}
 
 @router.put("/user/{id}",tags=["User"])
 def editUser(id:str,user:User, token: str = Depends(tokenAuthScheme)):
-    userData = verifyToken(token.credentials)
+    userData = verifyToken(token)
     checkExist = dict(userTable.find_one({"_id":{"$ne":ObjectId(id)},"$or":[{"mobile":user.mobile},{"email":user.email}]}))
     if checkExist:
         raise HTTPException(status_code=422,detail="Email or Mobile already exist")
@@ -121,12 +122,12 @@ def editUser(id:str,user:User, token: str = Depends(tokenAuthScheme)):
         "password":user.password
     }
     #user["isActive"] = True 
-    userTable.update_one({"_id":ObjectId(id)},dataToInsert)
+    userTable.update_one({"_id":ObjectId(id)},{"$set":dataToInsert})
     return {"success":True,"data":"User Update Success"}
 
 @router.post("/site",tags=["Site"])
 def addSite(site:Site, token: str = Depends(tokenAuthScheme)):
-    userData = verifyToken(token.credentials)
+    userData = verifyToken(token)
     dataToInsert = {
         "name":site.name,
         "code":site.code,
@@ -145,7 +146,7 @@ def addSite(site:Site, token: str = Depends(tokenAuthScheme)):
 
 @router.put("/site/{id}",tags=["Site"])
 def editSite(id:str,site:Site, token: str = Depends(tokenAuthScheme)):
-    userData = verifyToken(token.credentials)
+    userData = verifyToken(token)
     dataToInsert = {
         "name":site.name,
         "code":site.code,
@@ -158,12 +159,12 @@ def editSite(id:str,site:Site, token: str = Depends(tokenAuthScheme)):
         "updatedBy":userData.get("_id")
     }
     #user["isActive"] = True 
-    siteTable.update_one({"_id":ObjectId(id)},dataToInsert)
+    siteTable.update_one({"_id":ObjectId(id)},{"$set":dataToInsert})
     return {"success":True,"data":"Site Update Success"}
 
 @router.get("/site",tags=["Site"])
 def getSite(page:int = 1, limit:int = 10, token: str = Depends(tokenAuthScheme)):
-    verifyToken(token.credentials)
+    verifyToken(token)
     skip = (page - 1)*limit
     print(skip)
     data = list(siteTable.find({"isActive":True}).limit(limit).skip(skip)) 
@@ -173,28 +174,33 @@ def getSite(page:int = 1, limit:int = 10, token: str = Depends(tokenAuthScheme))
 
 @router.get("/site/user-mapped/{id}",tags=["Site"])
 def getSite(id:str, token: str = Depends(tokenAuthScheme)):
-    verifyToken(token.credentials)  
-    data = userTable.find({"isAdmin":False},{"_id":1,"name":1})
-    followData = userTable.find({"isAdmin":False,"sites":id},{"_id":1})
-    followIds = [el["_id"] for el in followData]
-    for el in data:
-        data["isMapped"] = el["_id"] in followIds
+    verifyToken(token)  
+    data = list(userTable.find({"isAdmin":False},{"_id":1,"name":1,"sites":True}))
+    print("data",data) 
+    for el in data:  
+        el["_id"] = str(el["_id"])
+        if el.get("sites"):
+            el["isMapped"] = bool(id in set(el.get("sites")))
+        else:
+            el["isMapped"] = False
     return {"success":True,"data":data}
 
 @router.post("/site/user-mapping",tags=["Site"])
 def userSiteMapping(site:SiteMapping, token: str = Depends(tokenAuthScheme)):
-    userData = verifyToken(token.credentials)
+    userData = verifyToken(token)
     dataToUpdate = {
         "$addToSet":{"sites":site.id},
-        "updatedAt": datetime.now(),
-        "updatedBy": userData.get("_id")
+        "$set":{
+            "updatedAt": datetime.now(),
+            "updatedBy": userData.get("_id")
+        }
     } 
     userTable.update_many({"_id":{"$in":site.userIds}},dataToUpdate)
     return {"success":True,"data":"Site Mapping Success"}
 
 @router.post("/qn-ans",tags=["QA"])
 def addQans(body:dict, token: str = Depends(tokenAuthScheme)): 
-    userData = verifyToken(token.credentials)
+    userData = verifyToken(token)
     body["createdBy"] = userData.get("_id")
     body["isActive"]= True
     body["createdAt"]= datetime.now()
@@ -203,7 +209,7 @@ def addQans(body:dict, token: str = Depends(tokenAuthScheme)):
 
 @router.get("/qn-ans",tags=["QA"])
 def getQuestionAnswer(page:int = 1, limit:int = 10, token: str = Depends(tokenAuthScheme)):
-    verifyToken(token.credentials)
+    verifyToken(token)
     skip = (page - 1)*limit
     print(skip)
     data = list(questionAnswersTable.find({"isActive":True}).limit(limit).skip(skip)) 
@@ -213,13 +219,13 @@ def getQuestionAnswer(page:int = 1, limit:int = 10, token: str = Depends(tokenAu
 
 @router.get("/qn-ans/{id}",tags=["QA"])
 def getgetQuestionAnswerById(id, token: str = Depends(tokenAuthScheme)):  
-    verifyToken(token.credentials)
+    verifyToken(token)
     data = dict(questionAnswersTable.find_one({"_id":ObjectId(id),"isActive":True}))  
     return {"success":True,"data":data}
 
 @router.post("/blob/upload",tags=["QA"])
 def uploadOnBlob(file:UploadFile, token: str = Depends(tokenAuthScheme)):  
-    verifyToken(token.credentials)
+    verifyToken(token)
     blobName = f"{datetime.now().timestamp()}-{file.filename}"
     blob = BlobClient.from_connection_string(conn_str=connectionString, container_name=containerName, blob_name=blobName) 
     blob.upload_blob(file.file) 
@@ -232,7 +238,7 @@ def verifyToken(token:str):
     userId = JWT().decrypt(token.credentials)
     if not userId or not userId.get("id"):
         raise unAuthError
-    userData = dict(userTable.find_one({"_id":ObjectId(userId.get("id"))}))
+    userData = userTable.find_one({"_id":ObjectId(userId.get("id"))})
     if not userData:
         raise unAuthError
     return userData
